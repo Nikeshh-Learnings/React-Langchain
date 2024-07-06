02cdbd096884e171ed9dc0aec8723e9973991256
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-05 21:48:18 -0400

    React langchain agent with tool implementation

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+from typing import Union, List
 from dotenv import load_dotenv
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.tools import tool
 from langchain.prompts.prompt import PromptTemplate
 from langchain.tools.render import render_text_description
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
+
 
 @tool
 def get_text_length(text: str) -> int:
     """Returns the length of the text by characters."""
     print(f"get_text_length enter with {text=}")
-    text = text.strip("'\n").strip('"')
+    text = text.strip("\n").strip('"')
+    print(f"get_text_length after striping {text=}")
     return len(text)
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")
 
 if __name__ == "__main__":
     print("Hello React Langchain!")
@@ -36,9 +48,26 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:{agent_scratchpad}
+    Thought:
     """
 
-    prompt = PromptTemplate.from_template(template=template).partial(tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools]))
-    llm = ChatOpenAI(temperature=0, stop=["\nObservation:"])
-    agent = prompt | llm
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+    llm = ChatOpenAI(temperature=0, stop=["Observation", "Final Answer"])
+    agent = (
+        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+    )
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {"input": "What is the length in characters of the text: DOG ?"}
+    )
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
